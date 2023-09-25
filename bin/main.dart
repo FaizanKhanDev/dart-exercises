f46b502323aa61647a7d4a7dc8582b707d11b3b2
FaizanKhanDev <faizan480khan@gmail.com>
2023-09-25 22:57:59 +0500

    do some multiplication

--- bin/main.dart
+++ bin/main.dart
@@ -6,4 +6,7 @@ void main() {
   // When you Don't want to Change Value of variable the you msut use final or const
   const getFullName = "my Full Name is Faizan khan";
   print(getFullName);
+  const bar = 1000000;
+  const atm = 1.01325 * bar;
+  print(atm);
 }
