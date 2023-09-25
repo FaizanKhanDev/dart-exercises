02e3459d53c1344a9d780a5474fbb6692b2d49ef
FaizanKhanDev <faizan480khan@gmail.com>
2023-09-25 22:55:16 +0500

    LEARNED; learned about final and late variables and const

--- bin/main.dart
+++ bin/main.dart
@@ -1,5 +1,9 @@
 void main() {
+  // When you want to Change Value of variable
   late String faizan;
   faizan = "Faizan Khan";
   print(faizan);
+  // When you Don't want to Change Value of variable the you msut use final or const
+  const getFullName = "my Full Name is Faizan khan";
+  print(getFullName);
 }
