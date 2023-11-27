e18db184aba0bd5200c64867d723b7b8d141a2f3
FaizanKhanDev <faizan480khan@gmail.com>
2023-11-27 02:01:27 -0800

    ADD LOOPS

--- bin/function.dart
+++ bin/function.dart
@@ -8,14 +8,30 @@ void main() {
 
   var faizan = mul(10, 50);
   if (faizan == 60) {
-    print("Faizan Khan");
+    // print("Faizan Khan");
   }
 
-  print(faizan);
+  // print(faizan);
 
   var khan = mul(10, 20);
   if (khan == 30) {
-    print("Khan");
+    // print("Khan");
   }
-  print(khan);
+  // print(khan);
+
+  List<String> list = ["Faizan", "Khan"];
+  list.forEach((element) {
+    // print(element);
+    // print('${list.indexOf(element)}: $element');
+  });
+
+  var faizanMap = {'age': 21, 'name': "Faizan", 'city': "Karachi"};
+  faizanMap.forEach((key, value) => {print("KEY: $key"), print("VAL: $value")});
 }
+
+// void faizan() {
+//   print("Faizan Khan");
+//   var fa = main();
+
+//   print(fa);
+// }
